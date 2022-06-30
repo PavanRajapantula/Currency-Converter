330209e87aa8e51d5dbe93f5b6f5557a4fe53acb
PavanRajapantula <72918948+PavanRajapantula@users.noreply.github.com>
2022-06-30 09:20:46 +0530

    Update script.js

--- Currency Converter website in JavaScript/Currency Converter App in JavaScript/js/script.js
+++ Currency Converter website in JavaScript/Currency Converter App in JavaScript/js/script.js
@@ -13,7 +13,7 @@ for (let i = 0; i < dropList.length; i++) {
         dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
     dropList[i].addEventListener("change", e =>{
-        loadFlag(e.target); // calling loadFlag with passing target element as an argument
+        loadFlag(e.target); 
     });
 }
 
@@ -59,10 +59,10 @@ function getExchangeRate(){
     let url = `https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/${fromCurrency.value}`;
    
     fetch(url).then(response => response.json()).then(result =>{
-        let exchangeRate = result.conversion_rates[toCurrency.value]; // getting user selected TO currency rate
-        let totalExRate = (amountVal * exchangeRate).toFixed(2); // multiplying user entered value with selected TO currency rate
+        let exchangeRate = result.conversion_rates[toCurrency.value]; 
+        let totalExRate = (amountVal * exchangeRate).toFixed(2); 
         exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
-    }).catch(() =>{ // if user is offline or any other error occured while fetching data then catch function will run
+    }).catch(() =>{ 
         exchangeRateTxt.innerText = "Something went wrong";
     });
 }
